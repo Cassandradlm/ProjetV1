372ad0e0d3b8782dc755da3d9e0584819c98d7c2
MaxLMTR <94896515+MaxLMTR@users.noreply.github.com>
2022-11-24 14:19:23 +0100

    firebase

--- app/src/main/java/com/example/projetv1/MainActivity.java
+++ app/src/main/java/com/example/projetv1/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.page_navigation);
+        setContentView(R.layout.page_categorie);
 
         navigationView = findViewById(R.id.bottomNavigationView);
         getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new TirageFragment()).commit();
