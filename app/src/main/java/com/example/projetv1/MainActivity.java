72a6d08b29328a92d2d69fd5c89c15a5d0e5387e
Cassandradlm <cassandra.dallemagne@esme.fr>
2022-11-28 17:15:20 +0100

    ok

--- app/src/main/java/com/example/projetv1/MainActivity.java
+++ app/src/main/java/com/example/projetv1/MainActivity.java
@@ -7,8 +7,10 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.MenuItem;
 
@@ -32,12 +34,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         setContentView(R.layout.page_navigation);
 
-        //a executer qu'une fois
-        extract_and_save ex = new extract_and_save();
-        ArrayList<Film> liste = ex.getListItems();
+        SharedPreferences pref = MainActivity.this.getSharedPreferences("FIRST",0);
+        SharedPreferences.Editor editor= pref.edit();
+        boolean firstRun = pref.getBoolean("firstRun", true);
+        if(firstRun)
+        {
+            extract_and_save ex = new extract_and_save();
+            ArrayList<Film> liste = ex.getListItems();
+            DBHandler dbHandler = new DBHandler(this);
+            for (int i = 0; i < liste.size(); i++) {
+                dbHandler.addNewCourse(
+                        liste.get(i).getNom(),
+                        liste.get(i).getDescription(),
+                        liste.get(i).getCategorie(),
+                        liste.get(i).getAffiche(),
+                        String.valueOf(liste.get(i).getDuree()),
+                        String.valueOf(liste.get(i).getAnnee()));
+                Log.d("OK FIRST","OKKK");
+            editor.putBoolean("firstRun",false);
+            editor.commit();
+            }
+        }
 
         navigationView = findViewById(R.id.bottomNavigationView);
         getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new TirageFragment()).commit();
--- app/src/main/java/com/example/projetv1/extract_and_save.java
+++ app/src/main/java/com/example/projetv1/extract_and_save.java
@@ -11,20 +11,23 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 
 public class extract_and_save {
+
     private static ArrayList<Film> mArrayList = new ArrayList<>();
 
-    private DBHandler dbHandler;
-    dbHandler = new DBHandler(MainActivity.this);
+
+
 
     ArrayList<Film> getListItems() {
         //a faire q'une fois
         ArrayList<Film> film_list = new ArrayList<>();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+
         db.collection("ProjetAndroid")
                 .get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
+
 
                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                         for (QueryDocumentSnapshot document: queryDocumentSnapshots){
