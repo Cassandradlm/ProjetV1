22976eb0e76bb14402b02403fa4c76a321af55b8
Cassandradlm <cassandra.dallemagne@esme.fr>
2022-11-28 12:10:16 +0100

    ok

--- app/src/main/java/com/example/projetv1/MainActivity.java
+++ app/src/main/java/com/example/projetv1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.projetv1;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -8,20 +9,35 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.MenuItem;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-
 
     BottomNavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         setContentView(R.layout.page_navigation);
+
+        //a executer qu'une fois
+        extract_and_save ex = new extract_and_save();
+        ArrayList<Film> liste = ex.getListItems();
 
         navigationView = findViewById(R.id.bottomNavigationView);
         getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new TirageFragment()).commit();
@@ -62,4 +78,5 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
     }
-}
+
+    }
